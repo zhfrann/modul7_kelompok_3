29775b880601b6a6093f39791ac4f8cec086c9f9
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-04-10 11:19:27 +0700

    Menambahkan class GlossaryItem

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
         DataMahasiswa103022300093.ReadJSON();
+        TeamMembers103022300093.ReadJSON();
+        GlossaryItem103022300093.ReadJSON();
     }
 }
