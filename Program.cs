540b8b13ddc5e672e547931db078cb4daa6a87d7
Epulvis <zuhrisaifuddin405@gmail.com>
2025-04-10 10:15:09 +0700

    Menambahkan class DataMahasiswa103022300057

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+DataMahasiswa103022300057.ReadJSON("jurnal7_1_103022300057.json");
