18c96f5f881e6bb9c39cadedfb9d5a7710484083
Epulvis <zuhrisaifuddin405@gmail.com>
2025-04-10 11:12:42 +0700

    menambahkan class GlossaryItem103022300057

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 DataMahasiswa103022300057.ReadJSON("jurnal7_1_103022300057.json");
 TeamMembers103022300057.ReadJSON("jurnal7_2_103022300057.json");
+GlossaryItem103022300057.ReadJSON("jurnal7_3_103022300057.json");
